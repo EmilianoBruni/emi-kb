e839c4302142c063e8b664bd36bdd2d711763f79
Emiliano Bruni <EmilianoBruni@users.noreply.github.com>
2017-11-25 19:23:36 +0100

    Create emi.kb.js

---
+++ emi.kb.js
@@ -0,0 +1,25 @@
+(function(Emi, $, undefined) {
+  Emi.kb = Emi.kb || {};
+
+  Emi.kb.Model = function(url, type) {
+    var mod = Backbone.Model.extend();
+    mod.prototype.urlRoot = url;
+    if (type === "mongo") {
+      mod.prototype.parse = function(data) {
+        var item = data[0] ? data[0] : data;
+        if (item) {
+          if (item._id) item.id = item._id["$oid"];
+        }
+        return item;
+      };
+    }
+    return mod;
+  };
+
+  Emi.kb.Collection = function(model) {
+    var coll = Backbone.Collection.extend();
+    coll.prototype.model = model;
+    coll.prototype.url = model.prototype.urlRoot;
+    return coll;
+  };
+})((window.Emi = window.Emi || {}), jQuery);
