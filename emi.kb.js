5949720a93f085857e5fe22d002cb6e743b17527
ebruni <info@ebruni.it>
2018-01-29 17:40:36 +0100

    Now 2nd parameter of Emi.kb is a JSON with type as old parameter and deferred that use Backbone.Deferred

--- emi.kb.js
+++ emi.kb.js
@@ -1,8 +1,12 @@
 (function(Emi, $, undefined) {
   Emi.kb = Emi.kb || {};
 
-  Emi.kb.Model = function(url, type) {
-    var mod = Backbone.Model.extend();
+  Emi.kb.Model = function(url, opt = {}) {
+    var type = opt.type || '';
+    var deferred = opt.deferred || false;
+    var mod = deferred
+        ? Backbone.Deferred.Model.extend()
+        : Backbone.Model.extend();
     mod.prototype.urlRoot = url;
     mod.prototype.parse = function(data) {
       var item = data[0] ? data[0] : data;
@@ -16,8 +20,11 @@
     return mod;
   };
 
-  Emi.kb.Collection = function(model) {
-    var coll = Backbone.Collection.extend();
+  Emi.kb.Collection = function(model, opt = {}) {
+    var deferred = opt.deferred || false;
+    var coll = deferred
+        ? Backbone.Deferred.Collection.extend()
+        : Backbone.Collection.extend();
     coll.prototype.model = model;
     coll.prototype.url = model.prototype.urlRoot;
     return coll;
