49a930f11a08981a1955f8fc1b8951aa4095f879
EmilianoBruni <info@ebruni.it>
2018-03-22 17:20:36 +0100

    More control over array parsing

--- emi.kb.js
+++ emi.kb.js
@@ -1,32 +1,41 @@
 (function(Emi, $, undefined) {
-  Emi.kb = Emi.kb || {};
+    Emi.kb = Emi.kb || {};
 
-  Emi.kb.Model = function(url, opt = {}) {
-    var type = opt.type || '';
-    var deferred = opt.deferred || false;
-    var mod = deferred
-        ? Backbone.Deferred.Model.extend()
-        : Backbone.Model.extend();
-    mod.prototype.urlRoot = url;
-    mod.prototype.parse = function(data) {
-      var item = data[0] ? data[0] : data;
-      if (type === "mongo") {
-        if (item) {
-          if (item._id) item.id = item._id["$oid"];
-        }
-      }
-      return item;
+    Emi.kb.Model = function(url, opt = {}) {
+        var type = opt.type || '';
+        var deferred = opt.deferred || false;
+        var mod = deferred ?
+            Backbone.Deferred.Model.extend() :
+            Backbone.Model.extend();
+        mod.prototype.urlRoot = url;
+        mod.prototype.parse = function(data) {
+            var item = data;
+            if (Array.isArray(item)) {
+                if (item.length === 0) {
+                    // it's a problem
+                    console.log('There is a problem');
+                    return {};
+                } else {
+                    item = item[0]
+                }
+            }
+            if (type === "mongo") {
+                if (item) {
+                    if (item._id) item.id = item._id["$oid"];
+                }
+            }
+            return item;
+        };
+        return mod;
     };
-    return mod;
-  };
 
-  Emi.kb.Collection = function(model, opt = {}) {
-    var deferred = opt.deferred || false;
-    var coll = deferred
-        ? Backbone.Deferred.Collection.extend()
-        : Backbone.Collection.extend();
-    coll.prototype.model = model;
-    coll.prototype.url = model.prototype.urlRoot;
-    return coll;
-  };
+    Emi.kb.Collection = function(model, opt = {}) {
+        var deferred = opt.deferred || false;
+        var coll = deferred ?
+            Backbone.Deferred.Collection.extend() :
+            Backbone.Collection.extend();
+        coll.prototype.model = model;
+        coll.prototype.url = model.prototype.urlRoot;
+        return coll;
+    };
 })((window.Emi = window.Emi || {}), jQuery);
